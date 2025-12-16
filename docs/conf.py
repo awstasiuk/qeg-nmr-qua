e5dba3f3cb34c73efdd74ea24ac853a21c3b7f98
Andrew Stasiuk <awstasiuk@gmail.com>
2025-12-16 17:02:36 -0500

    maybe fixed now?

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ extensions = [
 ]
 
 templates_path = ["_templates"]
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "README.md"]
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
